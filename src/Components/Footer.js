1d32f01cc97e6b60265b2b9b9f661a979aa71c2f
unknown <grandosung111@gmail.com>
2019-10-19 21:06:19 +0900

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,11 +10,64 @@ const Foot = styled.div`
     background-color:${props => props.theme.blueColor};
 `
 
+const Label = styled.div`
+    margin:13px;
+`
+
+
+const Box = styled.div`
+    display:flex;
+    justify-content:center;
+    margin:0 auto;
+    margin-top:36px;
+    width:${props => props.theme.maxWidth};
+    text-align:center;
+    background-color:red;
+`
+
+const InfoBox = styled.p`
+    margin:0 auto;
+    color:grey;
+    text-align:center;
+    width:${props => props.theme.maxWidth};
+    margin-top:32px;
+`
+const FooterLast = styled.div`
+    width:1200px;
+    background-color:#646566;
+    border-radius: 5px 5px 0px 0px;
+    position:absolute;
+    bottom:0;
+    margin:0 auto;
+
+
+`
 
 const Footer=() => (
 
     <Foot>
-        d
+      <Box>
+          사진이 들어갈 자리
+      </Box>
+      <Box>
+         <Label onClick={()=> {alert('ok')}}>회사소개</Label>
+         <Label>사업분야</Label>
+         <Label>주요실적</Label>
+         <Label>포토폴리오</Label>
+         <Label>온라인상담문의</Label>
+         <Label>고객센터</Label>
+
+
+      </Box>
+      <InfoBox>
+         경기도 부천시 원미구 중동 1152 상록센트럴타워 505호 샤이닝(Make24팀)<br/><br/>
+         사업자 번호 : 130-37-46318(등록2007년) 통신판매업종신고 : 제2010-경기부천-924호 상호 : 샤이닝 대표 : 오성민 <br/><br/>
+         고객센터:1544-0634 팩스:0505-200-6060 이메일:hdweb@hdweb.co.kr<br/><br/>
+      </InfoBox>
+      <FooterLast>
+           Copyright ⓒ 샤이닝 - 인테리어 홈페이지 샘플 All rights reserved.
+      </FooterLast>
+
     </Foot>
     
 )
--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -2,6 +2,7 @@ const BOX_BORDER = "1px solid #e6e6e6";
 const BORDER_RADIUS = "4px";
 
 export default {
+  normalWidth:"700px",
   maxWidth: "935px",
   bgColor: "#FAFAFA",
   blackColor: "#262626",
